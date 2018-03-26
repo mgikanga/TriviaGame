31402ab3ba37fb761c09b288988d6e330a74e3ca
Monicah Gikanga <mgikanga@Admins-MacBook-Air.local>
2018-03-26 16:59:38 -0700

    added an array for the options and buttons to display them on the webpage

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,39 +1,41 @@
-console.log("im inn");
-var time = 30;
+//my global variables
+var time = 31;
 var intervalId;
-var timerRunning = false;
 var answer;
 var userPick;
 
 //timer function
-$("button").on("click", function(){
+$("#buton").on("click", function () {
     $(this).hide();
-intervalId = setInterval(countDown,1000)
-display();
-    
-    })
-    //funtion to display text on webpage
-    function display(){
-        countDown();
-       $("#quiz").html("<h3>" + text+ "</h3>");
-       $("#quiz").append("<p>"+optionA+"</p>" + "<p>"+optionB+"</p>" +"<p>"+optionC+"</p>")
-     $("p").on("click" , function(){
-         
-     })
-    }
+    intervalId = setInterval(countDown, 1000)
+    display();
+
+})
+//funtion to display text on webpage
+function display() {
+    countDown();
+    $("#quiz").html("<h3>" + text + "</h3>");
+    for(var i=0; i<optionArr.length;i++){
+        $("#quiz").append("<button id = 'pick'>" + optionArr[i] + "</button>");
+        $("#pick").on("click", function () {
+   
+            
+        })
+        
+      console.log(optionArr[i]);
+      }      
+}
 
 //countdown function
-function countDown(){
+function countDown() {
     time--;
-   $("#clock").html("<h3>Time remaining: "+ time + " seconds</h3>")
-    console.log(time);
-    if(time<=0){
+    $("#clock").html("<h3>Time remaining: " + time + " seconds</h3>")
+    if (time <= 0) {
         $("#clock").html("You run out of time");
-return;
-        
+        return;
+
     }
-    }
-    
+}
 
 // object for questions
 var questions = [
@@ -57,18 +59,20 @@ var questions = [
         answer: "Phoenix",
         options: ["Helsinki", "Oslo", "Phoenix"],
     },
-   
+
 ];
 
 // For the Norway question
 var selected;
 var text = questions[0].text;
 var answer = questions[0].answer;
+var optionArr=questions[0].options;
 var optionA = questions[0].options[0]
-var optionB =questions[0].options[1]
+var optionB = questions[0].options[1]
 var optionC = questions[0].options[2]
 var optionD = questions[0].options[3]
 console.log(text);
+//compare the user pick and answer
 
 
 /* For The Hobbit related questoin
